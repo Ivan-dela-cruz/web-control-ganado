63b3719aa1210f3fc584330fc2add8f9726abc96
Ivan <canarivan495@gmail.com>
2021-01-15 20:31:17 -0500

    feat:(API) REVIEW SERVICE POST MILKING

--- app/Http/Controllers/Api/EstateController.php
+++ app/Http/Controllers/Api/EstateController.php
@@ -89,7 +89,7 @@ class EstateController extends Controller
 
     public function AnimalsProductionByMilking(Request $request, $id)
     {
-
+        
         //fechas
         $now = Carbon::now("America/Guayaquil");
         $date =  $now->format('Y-m-d');
@@ -100,11 +100,12 @@ class EstateController extends Controller
         ->where('animal_production.estate_id',$id)
         ->get();
 
+      
+
         //ANIMALES YA ORÑADOS
         $income = null;
-
         $income = Income::where('estate_id',$id)->where('date',$date)->where('time_milking',$request->time_milking)->first(['id']);
-
+       
         if(!is_null($income)){
             $animals_milinkgs = Milking::where('income_id',$income->id)->get(['id','animalproduction_id','income_id']);
             foreach( $animals as $k => $v){
@@ -115,7 +116,6 @@ class EstateController extends Controller
                 }
             }
         }
-
         return response()->json([
             'success'=>true,
             'animals'=>$animals,
@@ -132,13 +132,12 @@ class EstateController extends Controller
         $time = $now->format('H:i:s');
         $year = $now->year;
 
-        $estate = Estate::find($id);
         $income  = null;
         $income = Income::where('estate_id',$id)->where('date',$date)->where('time_milking',$request->time_milking)->first();
 
         if(is_null($income)){
             $income = new Income();
-            $income->estate_id = $estate->id;
+            $income->estate_id = $id;
             $income->year = $year;
             $income->date = $date;
             $income->hour = $time;
@@ -151,7 +150,7 @@ class EstateController extends Controller
             $income->save();
         }
 
-        $animal = Animal_production::where('animal_id',$request->animalproduction_id)->first();
+        $animal = Animal_production::find($request->animalproduction_id);
 
         $milking = new Milking();
         $milking->income_id = $income->id;
@@ -165,7 +164,7 @@ class EstateController extends Controller
         ///TOTAL DE LITROS SUMATORIA
         $income->total_liters = $income->total_liters + $request->total_liters;
         $income->save();
-        $this->AnimalsProductionByMilking($request, $id);
+        return $this->AnimalsProductionByMilking($request, $id);
     }
 
 
